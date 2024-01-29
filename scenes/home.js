d862bd70cc160099fdfdb32978abf627d054a713
ayo986 <140111670+ayo986@users.noreply.github.com>
2024-01-29 11:20:08 +0100

    Update home.js

--- scenes/home.js
+++ scenes/home.js
@@ -1,11 +1,23 @@
 
 let scene = new Scene(0, 0, width, height)
+let im , txt, data
+let pos
+scene.setup = function() {
+    this.childs = []
+    this.bg = [10]
+    pos = new vec2(cx, cy)
+
+}
+
 scene.draw = function(){
-    
+    lg.shadow('ff0000', 100)
+    lg.setColor(rgba(255))
+   // lg.print('aaaaa', cx, cy)
+    lg.circle('fill', pos.x, pos.y, 50)
+    pos.add(new vec2(1, 0))
+    lg.shadow()
 }
-scene.setup = function(){
-    
-}
+
 scene.resize = function(w, h){
     this.width = w
     this.height = h

5cc472295e387b33f88d5c515d9cce227055d593
ayo986 <140111670+ayo986@users.noreply.github.com>
2024-07-07 16:47:21 +0200

    Add files via upload

--- scenes/home.js
+++ scenes/home.js
@@ -1,151 +1,140 @@
-
 let scene = new Scene()
+
 function getCount(t){
-    let n = 0
-    for (let o of t) {
-        if (o.state) {
-            n++
-        }
-    }
-    return n 
+	let n = 0
+	for (let o of t) {
+		if (o.state) {
+			n++
+		}
+	}
+	return n 
 }
 
 function copyTxt(nodes, n, str, p){
-    n = n || 0
-    p = p || ''
-    str = str || ''
-    let j = 0
-    for (let node of nodes || []) {
-        if (node.childs && getCount(node.childs) > 0) {
-            str = str + '*' + p + '(' + node.text + ')*\r\n'
-        }
+	n = n || 0
+	p = p || ''
+	str = str || ''
+	let j = 0
+	for (let node of nodes || []) {
+		if (node.childs && getCount(node.childs) > 0) {
+			str = str + '*' + p + '(' + node.text + ')*\r\n'
+		}
 
-        if (node.state) {
-            j++
-            let nr = 16 - node.text.length
-            let hh = ''
-            // if (nr > 0) {
-            //    hh = ' '.repeat(nr)
-            // }
-            str = str + j + '-' + node.text + hh + '\r\n'
-        }
+		if (node.state) {
+			j++
+			let nr = 16 - node.text.length
+			let hh = ''
+			// if (nr > 0) {
+			//    hh = ' '.repeat(nr)
+			// }
+			str = str + j + '-' + node.text + hh + '\r\n'
+		}
 
-        if (node.childs) {
-           str = copyTxt(node.childs, n + 1, str, node.text)
-        }
-    }
+		if (node.childs) {
+		   str = copyTxt(node.childs, n + 1, str, node.text)
+		}
+	}
 
-    return str
+	return str
 }
+
 function addElement(node, n){
-    let mc = new moveClip(0, 0, 10, 10)
-    mc.hasBg = false
-    let im = node.fileImage ? loadImage(node.fileImage) : null 
+    let im = node.fileImage ? newImage(node.fileImage) : null 
     let itemColor = this.itemColor
-    let o = new radioButton('', 0, 0, 100, 100)
-    
-    o.width = this.width
-    o.height = this.getItemSize()-this.space
-    // o.origin = CENTER_CENTER
-    // o.origin = TOP_LEFT
+    let o = node.radio ? new radioButton() : new Button()
     o.set(node)
-    o.radio = node.radio
     o.childs = []
     o.imageIcon = im
-    o.alignText = 'right'
     o.fg = this.fg
     o.bg = [itemColor[0] + 20*n, itemColor[1] + 20*n, itemColor[2] + 20*n]
     o.node = node
-    o.vater = this
-    o.state = node.state
-    o.check = function() {
-        return !o.vater.moving
-    }
-    if (node.radio) {
-        o.onChange = function(state) {
-            this.node.state = state
-        }
-    }
-
-    if (node.childs) {
+    o.fontSize = 14
+    o.alignText = this.alignText
+    o.corner = [0]
+    if (!node.radio) {
         o.mousereleased  = function(x, y, b){
-            let p = this.vater
+            let p = this.getParent()
             if (b == 1) {
                 if (!p.moving) {
-                    this.node.expanded = !this.node.expanded
+                    if (this.node.childs) {
+                        this.node.expanded = !this.node.expanded
+                    }
+                    
+                    p.onSelected(this, this.node)
                     p.updateData()
                 }
             }
         }
     }
-    mc.insert(o)
-    return mc
+    if (node.radio) {
+        o.state = node.state
+        o.onChange = function(state) {
+            this.node.state =state
+        }
+    }
+
+    return o
 }
 scene.setup = function(){
     this.childs = []
-    // this.bg = [100]
-    let im = lg.loadImage('res/icons/1.png')
-
-
+    this.clip = false
     let ebar = new explorThumb(0, 0, width, height - 40)
-    ebar.align = 'left'
-    ebar.addElement = addElement
-    ebar.bg = [style.bg[0]/2, style.bg[1]/2, style.bg[2]/2]
-    ebar.space = 2
-    this.insert(ebar)
-    ebar.setDisplayItem(14)
-    ebar.setIndex(1)
-
-    loadJson('db.json', function(data) {
-        let db = data
+    loadJSON('db.json', data =>{
+        ebar.addElement = addElement
+        ebar.space = 4
+        ebar.direction = 'v'
+        ebar.align = 'left'
+        ebar.alignText = 'right'
+        // ebar.bg = [20]
+        ebar.setDisplayItem(16)
+        ebar.setIndex(1)
+        this.insert(ebar)
         ebar.nodes = data
         ebar.updateData()
     })
 
-    let lv = new listView(0, height - 40, width, 40)
-    lv.direction = 'h'
-    lv.bg = [20]
-    lv.space = 0
-    lv.origin = TOP_LEFT
-    this.insert(lv)
-
-    let buttons = [
+    let btns = [
         {
-            text: "Copy List", 
-            imageIcon:lg.newImage('res/icons/share.png'),
+            text: "CopyText", 
+            imageIcon: newImage('res/icons/share.png'),
             mousepressed: function(x, y, b){
-                this.opacity = 50
-            },
-            mousereleased: function(x, y, b){
-                this.opacity = 100
-                let sRes = copyTxt(ebar.nodes)
-                navigator.clipboard.writeText(sRes + '\r\n' + 'Ahmed Younis');
-                alert('done')
-                // print(sRes)
-            },
+				this.opacity = 50
+			},
+			mousereleased: function(x, y, b){
+				this.opacity = 100
+				let sRes = copyTxt(ebar.nodes)
+                navigator.clipboard.writeText(sRes)
+				alert('done')
+				// print(sRes)
+			},
         },
         {
-            text: "About", 
-            imageIcon:lg.newImage('res/icons/info.png'), 
-            alignText: "right",
+            text: "info",
+            imageIcon: newImage('res/icons/info.png'), 
+            alignText: 'right',
             mousepressed: function(x, y, b){
-                this.opacity = 50
-            },
-            mousereleased: function(x, y, b){
-                this.opacity = 100
-                alert('Ahmed Younis Elisabethstr.18A 24143 kiel')
+				this.opacity = 50
+			},
+			mousereleased: function(x, y, b){
+				this.opacity = 100
+				alert('Ahmed Younis Elisabethstr.18A 24143 kiel')
 
-            },
+			},
         },
+            
     ]
-
-    for (let i in buttons) {
-        let b = buttons[i]
-        let o = new Button()
-        o.set(b)
-        lv.addItem(o)
+    let tbtns = new listView(0, height - 40, width, 40)
+    tbtns.origin = TOP_LEFT
+    tbtns.direction = 'h'
+    // tbtns.bg = [10]
+    tbtns.space = 0
+    tbtns.setDisplayItem(2)
+    for (let t of btns) {
+        let b = new Button()
+        b.set(t)
+        tbtns.addItem(b)
     }
-    lv.setDisplayItem(2)
+    this.insert(tbtns)
+
 }
 return scene
-
